0f0edb744e518fd540385f33006555a78c07a2c0
JimJZY <103918693+JimJZY@users.noreply.github.com>
2022-08-03 12:02:03 +0800

    Add files via upload

---
+++ tk42.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import filedialog
+
+root = Tk()
+
+def callback():
+    fileName = filedialog.askopenfilename(filetypes=[("PNG",".png"),("GIF",".gif"),("JPG",".jpg"),("Python",".py")])
+    print(fileName)
+
+Button(root,text="打开文件",command=callback).pack()
+
+    
+mainloop()
